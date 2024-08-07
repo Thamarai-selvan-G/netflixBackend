39929d181c511f11617f33f20e44f8cababf61a2
Thamarai-selvan-G <thamaraiselvan878@gmail.com>
2024-08-07 16:18:42 +0530

    admin corrections complete

--- Controller/admin.controller.js
+++ Controller/admin.controller.js
@@ -7,46 +7,90 @@ const { where } = require("sequelize");
 var secret = "123456789";
 var salt = bcrypt.genSaltSync(10);
 
-let createAdmin = async (req, res) => {
+let adminCreate = async (req, res) => {
   try {
     let data = req.body;
     let inputEmail = req.body.email;
     let inputPassword = req.body.password;
 
-    let checkUserEmail = await adminController.findAll({where: { email: inputEmail }});
-    
-    if (!checkUserEmail.length == 0) {
+    let checkUserEmail = await adminController.findAll({
+      where: { email: inputEmail },
+    });
+
+    if (checkUserEmail.length > 0) {
       return res.json({ message: "account alredy created " });
     }
 
     let hash = await bcrypt.hash(inputPassword, salt);
     data.password = hash;
 
-    const create = await adminController.create(data);
+    const created = await adminController.create(data);
 
-    if (!create) {
+    if (!created) {
       return res.status(500).json({ message: "internal server error..." });
     }
 
-    const genToken = await jwt.sign({ id: data.id }, secret, {
-      expiresIn: "2hr",
-    });
+    const genToken = jwt.sign({ id: created.id }, secret, { expiresIn: "2hr" });
 
     const updateAdmin = await adminController.update(
-      { token: genToken },
-      { where: { id: data.id } }
+      { tocken: genToken },
+      { where: { id: created.id } }
     );
 
     if (!updateAdmin) {
       return res.status(500).json({ message: "server error..." });
     }
 
-    return res.status(201).json({ message: "admin created succesfully...", result: data });
+    return res.status(201).json({ message: "admin created succesfully..." });
   } catch (error) {
     return res.json({ message: error.message });
   }
 };
 
+let findAll = async (req, res) => {
+  try {
+    const data = await adminController.findAll();
+    if (!data) {
+      return res.status(404).json({ message: "data not found" });
+    }
+    return res.status(200).json({ message: data });
+  } catch (error) {
+    return res.json({ message: error.message });
+  }
+};
+
+let loginAdmin = async (req, res) => {
+  const inputEmail = req.body.email;
+  const inputPassword = req.body.password;
+
+  let checkUser = await adminController.findOne({
+    where: { email: inputEmail },
+  });
+  if (!checkUser) {
+    return res.status(404).json({ message: "sign in to continue" });
+  }
+  let comparePassword = await bcrypt.compare(inputPassword, checkUser.password);
+  if (!comparePassword) {
+    return res.json({ message: "incorrect password" });
+  }
+
+  const genToken = jwt.sign({ id: checkUser.id }, secret, { expiresIn: "2hr" });
+  const result = await adminController.update(
+    { token: genToken },
+    { where: { id: checkUser.id } }
+  );
+
+  if (!result) {
+    return res.status(500).json({ message: "internal error" });
+  }
+
+  return res
+    .status(200)
+    .json({ message: "logged In succesfull", result: result });
+};
+
 module.exports = {
-  createAdmin,
+  adminCreate,
+  loginAdmin,
+  findAll,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const adminController = require('../Controller/admin.controller')
 
-router.post('/create',adminController.createAdmin)
+router.post('/create',adminController.adminCreate)
+router.get('/viewall',adminController.findAll)
+router.post('/login',adminController.loginAdmin)
+
 
 module.exports = router;

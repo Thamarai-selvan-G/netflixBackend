fbcee24ddab2ca80a22133110c2a6db484749bac
Thamarai-selvan-G <thamaraiselvan878@gmail.com>
2024-08-22 11:02:12 +0530

    config file correction

--- Controller/vendor.controller.js
+++ Controller/vendor.controller.js
@@ -9,26 +9,26 @@ let createVendor = async (req, res) => {
     let data = req.body;
     let inputEmail = req.body.email;
 
-    await vendorController.findAll({ where: { email: inputEmail } })
+    await vendorController
+      .findAll({ where: { email: inputEmail } })
       .then(async (val) => {
         if (val.length == 0) {
           var salt = bcrypt.genSaltSync(10);
           var hash = await bcrypt.hash(req.body.password, salt);
           data["password"] = hash;
-          await vendorController.create(data)
-            .then(async (value) => {
-              let token = jwt.sign({ id: value.id }, secret, {
-                expiresIn: "2h",
-              });
-              await vendorController.update(
-                { token: token },
-                { where: { id: value.id } }
-              );
-              return res.json({
-                status: 200,
-                message: "account created successfully",
-              });
+          await vendorController.create(data).then(async (value) => {
+            let token = jwt.sign({ id: value.id }, secret, {
+              expiresIn: "2h",
             });
+            await vendorController.update(
+              { tocken: token },
+              { where: { id: value.id } }
+            );
+            return res.json({
+              status: 200,
+              message: "account created successfully",
+            });
+          });
         } else {
           return res.json({
             status: 500,
@@ -147,26 +147,31 @@ const loginId = async (req, res) => {
   try {
     let inputEmail = req.body.email;
     let inputPassword = req.body.password;
-    const user = await vendorController.findOne({ where: { email: inputEmail } });
+    const user = await vendorController.findOne({
+      where: { email: inputEmail },
+    });
     if (!user) {
       return res.json({
         status: 404,
-        message: 'signIn to continue'
+        message: "signIn to continue",
       });
     }
     const checkPassword = await bcrypt.compare(inputPassword, user.password);
     if (!checkPassword) {
       return res.json({
         status: 401,
-        message: 'incorrect password'
+        message: "incorrect password",
       });
     }
-    let newToken = jwt.sign({ id: user.id }, secret, { expiresIn: '2hr' });
-    let updateToken = await vendorController.update({ token: newToken }, { where: { id: user.id } });
+    let newToken = jwt.sign({ id: user.id }, secret, { expiresIn: "2hr" });
+    let updateToken = await vendorController.update(
+      { token: newToken },
+      { where: { id: user.id } }
+    );
     if (updateToken) {
       return res.json({
         status: 200,
-        message: 'login successful'
+        message: "login successful",
       });
     }
   } catch (error) {
@@ -178,30 +183,34 @@ const delectAcc = async (req, res) => {
   try {
     let inputEmail = req.body.email;
     let inputPassword = req.body.password;
-    const user = await vendorController.findOne({ where: { email: inputEmail } });
+    const user = await vendorController.findOne({
+      where: { email: inputEmail },
+    });
     if (!user) {
       return res.json({
         status: 404,
-        message: 'account not found'
+        message: "account not found",
       });
     }
     let checkPassword = await bcrypt.compare(inputPassword, user.password);
     if (!checkPassword) {
       return res.json({
         status: 401,
-        message: 'incorrect password'
+        message: "incorrect password",
       });
     }
-    let deleteUser = await vendorController.destroy({ where: { email: inputEmail } });
+    let deleteUser = await vendorController.destroy({
+      where: { email: inputEmail },
+    });
     if (deleteUser) {
       return res.json({
         status: 200,
-        message: 'account deleted successfully'
+        message: "account deleted successfully",
       });
     } else {
       return res.json({
         status: 500,
-        message: 'internal Server error'
+        message: "internal Server error",
       });
     }
   } catch (error) {
@@ -216,5 +225,5 @@ module.exports = {
   updateId,
   deletedata,
   loginId,
-  delectAcc
-}
+  delectAcc,
+};
